c0c871a5d0b43c91e2ece788e7921d0887fe2fce
Suraj Rampure <surajr@me.com>
2023-05-10 11:58:33 +0200

    first push slides

---
+++ slides/tests/q1_3.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1_3',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> callable(a_cubed_plus_b_squared)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> a_cubed_plus_b_squared(5, 2) == 129\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
